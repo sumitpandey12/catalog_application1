a6472787745814ed2591ca984ba457039aff6806
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-05-29 15:24:29 +0530

    1 hour/

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:catalog_application1/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => HomeApp(),
+        "/login": (context) => LoginPage()
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Catalog"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
